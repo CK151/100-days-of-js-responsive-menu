138c9afbce0937512b73415704d6ef450af4d153
CK151 <andersonoki15@gmail.com>
2022-11-20 02:39:27 +0300

    init

---
+++ script.js
@@ -0,0 +1,42 @@
+//smooth scrolling
+
+const links = document.querySelectorAll(".nav-list li a");
+
+for (i of links) {
+    i.addEventListener("click", smoothScroll);
+}
+
+
+function smoothScroll(e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    document.querySelector(href).scrollIntoView({behavior: "smooth",
+
+});
+
+hideMenu()
+
+}
+
+
+//RESPONSIVE MOBILE MENU
+
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".harmburger");
+const close = document.querySelector(".close");
+
+const showMenu = () => {
+    hamburger.style.display = "none";
+    close.style.transform = "translateY(0)";
+    menu.style.transform =  "translateY(0)";
+};
+
+const hideMenu = () => {
+    close.style.transform = "translateY(-20rem)";
+    hamburger.style.display = "block";
+    menu.style.transform = "translateY(-200%)";
+}
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
